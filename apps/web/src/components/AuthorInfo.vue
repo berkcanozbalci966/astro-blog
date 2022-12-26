912a01016dc482c28d2218f761d7efea4eedc6e3
DSK1113827 <Berkcan.Ozbalci@agesa.com.tr>
2022-12-26 18:34:26 +0300

    blog image added & author info component created

---
+++ apps/web/src/components/AuthorInfo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type UserDetail = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  domain: string;
+};
+const { userDetail } = defineProps<{
+  userDetail: UserDetail;
+}>();
+
+const { firstName, lastName, email, domain } = userDetail;
+</script>
+
+<template>
+  <div className="mt-5">
+    <p>Author: {{ `${firstName} ${lastName}` }}</p>
+    <p>E-mail: {{ email }}</p>
+    <p>Web site: {{ domain }}</p>
+  </div>
+</template>
+
+<style scoped></style>
