bcfe0766b94327b3209d283c6361c286145c721b
DSK1113827 <Berkcan.Ozbalci@agesa.com.tr>
2022-12-26 19:18:04 +0300

    post comments created & tsconfig configurated for react

---
+++ apps/web/src/components/Comment.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+type CommentDetail = {
+  body: string;
+  user: { username: string };
+};
+const { commentDetail, index } = defineProps<{
+  commentDetail: CommentDetail;
+  index: number;
+}>();
+
+const { body, user } = commentDetail;
+</script>
+
+<template>
+  <div :class="['chat', index % 2 === 1 ? 'chat-start' : 'chat-end']">
+    <div class="chat-image avatar">
+      <div class="w-10 rounded-full">
+        <img src="https://placeimg.com/192/192/people" />
+      </div>
+    </div>
+    <div class="chat-header">
+      {{ user.username }}
+    </div>
+    <div class="chat-bubble">{{ body }}</div>
+  </div>
+</template>
+
+<style scoped></style>
